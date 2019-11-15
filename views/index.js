45f14d5de8f84bce9c383dc429cf7d3487a7a3b9
unknown <phongquocpham0002@gmail.com>
2019-11-15 21:16:10 +0700

    them nhanh test

--- views/index.js
+++
@@ -1,9 +0,0 @@
-import homeScreen from "./home.js";
-const setScreen = function(screen)
-{
-    document.getElementById("app").innerHTML = screen.content;
-    screen.onload();
-}
-setScreen(homeScreen);
-
-export default setScreen;
